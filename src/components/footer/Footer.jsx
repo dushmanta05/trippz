59c774ade31d006eb5fd7d4eddf9f19361b91e93
Dushmanta <dushmanta0511@gmail.com>
2023-12-14 23:26:12 +0530

    Create separate data file for data and links, format code

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./style.css";
+import { socialsData, linksData } from "./FooterData";
 
 const Footer = () => {
   return (
@@ -12,48 +13,22 @@ const Footer = () => {
           <p>Choose your favourite destination.</p>
         </div>
         <div className="socials">
-          <a href="/">
-            <i className="fa-brands fa-square-facebook"></i>
-          </a>
-          <a href="/">
-            <i className="fa-brands fa-square-instagram"></i>
-          </a>
-          <a href="/">
-            <i className="fa-brands fa-square-twitter"></i>
-          </a>
-          <a href="/">
-            <i className="fa-brands fa-linkedin"></i>
-          </a>
+          {socialsData.map((item) => (
+            <a href={item.href}>
+              <i className={item.className}></i>
+            </a>
+          ))}
         </div>
       </div>
       <div className="bottom">
-        <div className="project">
-          <h4>Project</h4>
-          <a href="/">Changelog</a>
-          <a href="/">Status</a>
-          <a href="/">License</a>
-          <a href="/">All Versions</a>
-        </div>
-        <div className="community">
-          <h4>Community</h4>
-          <a href="/">GitHub</a>
-          <a href="/">Issues</a>
-          <a href="/">Project</a>
-          <a href="/">FAQs</a>
-        </div>
-        <div className="help">
-          <h4>Help</h4>
-          <a href="/">Support</a>
-          <a href="/">Trip Guide</a>
-          <a href="/">Contact Us</a>
-        </div>
-        <div className="others">
-          <h4>Others</h4>
-          <a href="/">Terms of Service</a>
-          <a href="/">Privacy</a>
-          <a href="/">Policy</a>
-          <a href="/">License</a>
-        </div>
+        {linksData.map((item) => (
+          <div className={item.className}>
+            <h4>{item.heading}</h4>
+            {item.links.map((link) => (
+              <a href={link.href}>{link.text}</a>
+            ))}
+          </div>
+        ))}
       </div>
     </div>
   );
